df2979c0ad98c182d459fa159e91693b2e88d1f9
phuongvo1110 <vptphuong21@clc.fitus.edu.vn>
2025-07-11 00:25:44 +0700

    Adding Mongodb

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const { mongoURI, cookieKey } = require("./config/key");
+
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(mongoURI);
+
 const app = express();
 
+app.use(
+    session({
+        secret: cookieKey,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
 const PORT = process.env.PORT || 3000;
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
+app.listen(PORT, () => {
+    console.log("Listening on port", PORT);
 });
---
+++ services/passport.js
@@ -0,0 +1,32 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const { googleClientID, googleClientSecret } = require("../config/key");
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => done(null, user));
+});
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: googleClientID,
+            clientSecret: googleClientSecret,
+            callbackURL: "/auth/google/callback",
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ googleId: profile.id }).then((existingUser) => {
+                if (!existingUser) {
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then((user) => done(null, user));
+                } else {
+                    done(null, existingUser);
+                }
+            });
+        }
+    )
+);
