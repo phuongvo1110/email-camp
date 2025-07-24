8b0ab4b8782e4a6bba5c28948a9d2ca72b8fd435
phuongvo1110 <vptphuong21@clc.fitus.edu.vn>
2025-07-24 21:26:31 +0700

    Update emailQueues.js

--- queues/emailQueues.js
+++ queues/emailQueues.js
@@ -5,5 +5,8 @@ const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
 const redisOption = {
     redis: { host: REDIS_HOST, port: REDIS_PORT, password: REDIS_PASSWORD },
 };
-const emailQueue = new Queue("email", redisOption);
+const emailQueue = new Queue(
+    "email",
+    process.env.NODE_ENV === "production" ? process.env.REDIS_URL : redisOption
+);
 module.exports = emailQueue;
